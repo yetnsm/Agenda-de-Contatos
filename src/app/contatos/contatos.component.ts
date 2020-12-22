fbffd184f3230339a7f80c7bc92203bdd9a99698
yetnsm <pericles.peron@hotmail.com>
2020-12-22 00:04:50 -0300

    com validacao de email e telefone

--- src/app/contatos/contatos.component.ts
+++ src/app/contatos/contatos.component.ts
@@ -61,10 +61,29 @@ export class ContatosComponent implements OnInit {
 
   adicionar(n: HTMLInputElement , e: HTMLInputElement, t: HTMLInputElement, a: HTMLInputElement): void {
     if (n.value != "" && e.value != "" && t.value != "" && a.value != "") {
-      let novo = [n.value, e.value, t.value, a.value]
-      this.contatos.push(novo)
-      this.quantContatos = this.contatos.length;
+      if(this.validarEmail(e.value) == false) {
+        alert("Insira um e-mail válido.\n "
+                +"Ex.: eduardosilva@hotmail.com");
+      } else if(this.validarTelefone(t.value) == false) {
+        alert("Formato de telefone inválido.\n "
+                +"Ex.: (84) 9 8888-8888");
+      } else {
+        let novo = [n.value, e.value, t.value, a.value]
+        this.contatos.push(novo)
+        this.quantContatos = this.contatos.length;
+      }
+    } else {
+      alert("Preencha todos os campos do formulário.");
     }
   }
 
+  validarEmail(email){
+    var regex = new RegExp(/^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?/).test(email);
+    return regex;
+  }
+
+  validarTelefone(telefone){
+    var regex = new RegExp(/^(\+{0,})(\d{0,})([(]{1}\d{1,3}[)]{0,}){0,}(\s?\d+|\+\d{2,3}\s{1}\d+|\d+){1}[\s|-]?\d+([\s|-]?\d+){1,2}(\s){0,}$/gm).test(telefone);
+    return regex;
+  }
 }
